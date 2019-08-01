042506b7d2554ede9f525b22297f2d0850312a27
Bortolinf <bortolin@yahoo.com>
2019-07-31 23:04:09 -0300

    chat2 - UFA. agora consegui me logar. UHULLLLLLL

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,230 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/widgets.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
-void main(){
-  Firestore.instance.collection("teste").document("teste").setData({"teste" : "teste"});
+void main() {
+  Firestore.instance
+      .collection("teste")
+      .document("teste")
+      .setData({"teste": "teste"});
+
   runApp(MyApp());
-
 }
+
+final ThemeData kIOSTheme = ThemeData(
+    primarySwatch: Colors.orange,
+    primaryColor: Colors.grey[100],
+    primaryColorBrightness: Brightness.light);
+
+final ThemeData kDefaultTheme = ThemeData(
+  primarySwatch: Colors.purple,
+  accentColor: Colors.orangeAccent[400],
+);
+
+
+
+// autentia no google e depois no firebase, tá ligado
+Future<Null> _ensureLoggedIn() async {
+  GoogleSignInAccount user = googleSignin.currentUser;
+  if(user == null)
+    user = await googleSignin.signInSilently();
+  if (user == null)
+    user = await googleSignin.signIn();
+  if (await auth.currentUser() == null) {
+    GoogleSignInAuthentication credentials = await googleSignin.currentUser.authentication;
+    await auth.signInWithCredential(GoogleAuthProvider.getCredential(
+        idToken: credentials.idToken, accessToken: credentials.accessToken));    
+  }  
+}
+
+
+_handleSubmited(String text) async {
+  await _ensureLoggedIn();
+  _sendMessage(text:text);
+}
+
+void _sendMessage({String text, String imgUrl}){
+  Firestore.instance.collection("messages").add(
+    {
+      "text" : text,
+      "imgUrl" : imgUrl,
+      "senderName" : googleSignin.currentUser.displayName,
+      "senderPhotoUrl" : googleSignin.currentUser.photoUrl
+    });
+}
+
+
+final googleSignin = GoogleSignIn();
+final auth = FirebaseAuth.instance;
+
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return MaterialApp(
+      title: "Chat App",
+      debugShowCheckedModeBanner: false,
+      theme: Theme.of(context).platform == TargetPlatform.iOS
+          ? kIOSTheme
+          : kDefaultTheme,
+      home: ChatScreen(),
     );
   }
 }
+
+class ChatScreen extends StatefulWidget {
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  @override
+  Widget build(BuildContext context) {
+    // SafeArea serve p/ignorar barrinha em baixo no IOS
+    return SafeArea(
+      bottom: false,
+      top: false,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Chat App"),
+          centerTitle: true,
+          elevation:
+              Theme.of(context).platform == TargetPlatform.iOS ? 0.0 : 4.0,
+        ),
+        body: Column(
+          children: <Widget>[
+            Expanded(
+              child: ListView(
+                children: <Widget>[
+                  ChatMessage(),
+                  ChatMessage(),
+                  ChatMessage(),
+              ],)
+               ,),
+            Divider(height: 1.0,),
+            Container(
+              decoration: BoxDecoration(
+                color: Theme.of(context).cardColor,
+              ),
+              child: TextComposer(),
+            )
+          ],
+        ),
+        
+      ),
+    );
+  }
+}
+
+class TextComposer extends StatefulWidget {
+  @override
+  _TextComposerState createState() => _TextComposerState();
+}
+
+class _TextComposerState extends State<TextComposer> {
+  
+  bool _isComposing = false;
+  final _textController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return IconTheme(
+      data: IconThemeData(color: Theme.of(context).accentColor),
+      child: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 8.0),
+        decoration: Theme.of(context).platform == TargetPlatform.iOS
+            ? BoxDecoration(
+                border: Border(top: BorderSide(color: Colors.grey[200])))
+            : null,
+        child: Row(
+          children: <Widget>[
+            Container(
+              child: IconButton(
+                icon: Icon(Icons.photo_camera),
+                onPressed: () {},
+              ),
+            ),
+            Expanded(
+              child: TextField(
+                controller: _textController,
+                decoration:
+                    InputDecoration.collapsed(hintText: "Enviar uma Mensagem"),
+                onChanged: (text) {
+                  _isComposing = text.length > 0;
+                },
+                onSubmitted:(text) { 
+                  _handleSubmited(text);
+                }
+              ),
+            ),
+            Container(
+                margin: const EdgeInsets.symmetric(horizontal: 4.0),
+                child:
+                   Theme.of(context).platform == TargetPlatform.iOS
+                    ? CupertinoButton(
+                        child: Text("Enviar"),
+                        onPressed: _isComposing
+                            ? () {
+                              _handleSubmited(_textController.text);
+                            }
+                            : null, // para o botao ficar desabilitado
+                      )
+                    : IconButton(
+                        icon: Icon(Icons.send),
+                        onPressed: _isComposing
+                            ? () {
+                              _handleSubmited(_textController.text);
+                            }
+                            : null, // para o botao ficar desabilitado
+                      )
+                      )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+class ChatMessage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+        Container(
+          margin: const EdgeInsets.only(right: 16.0),
+          child: CircleAvatar(
+            backgroundImage: NetworkImage("https://i.udemycdn.com/user/50x50/28039900_9aef.jpg"),
+          ),
+        ),
+        Expanded(
+          child:
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              //mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+              Text(
+                "Daniel",
+                style: Theme.of(context).textTheme.subhead,
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 5.0),
+                child: Text("alal la la lom, alala lala lom"),
+              )
+            ],)
+        )
+      ],),
+    );
+  }
+}
+
+
+
+
+// atualizacao
